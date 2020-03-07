6aaf4b8570f2380232e2b55157b7c4d2494bdd43
wanghanqi <2268133030@qq.com>
2020-03-08 00:22:41 +0800

    按钮优化，增加答案之书功能

--- app/src/main/java/com/example/wanghanqi/myapplication/fragment/TwoChoiceFragment.java
+++ app/src/main/java/com/example/wanghanqi/myapplication/fragment/TwoChoiceFragment.java
@@ -6,18 +6,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.wanghanqi.myapplication.R;
 import com.example.wanghanqi.myapplication.bean.Constance;
+import com.example.wanghanqi.myapplication.bean.WordsMap;
 import com.example.wanghanqi.myapplication.utils.VLog;
 import com.example.wanghanqi.myapplication.widget.LuckPan;
 import com.example.wanghanqi.myapplication.widget.LuckPanAnimEndCallBack;
+
+import java.util.Random;
 
 public class TwoChoiceFragment extends BaseFragment {
 
     private LuckPan pan;
     private ImageView imgStart;
+    private TextView mAnswer;
+    private Random random;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,10 +36,13 @@ public class TwoChoiceFragment extends BaseFragment {
     private void initView(View root) {
         pan = (LuckPan) root.findViewById(R.id.pan);
         imgStart = (ImageView) root.findViewById(R.id.img_start);
+        random = new Random();
         pan.setLuckPanAnimEndCallBack(new LuckPanAnimEndCallBack() {
             @Override
             public void onAnimEnd(String str) {
                 Toast.makeText(mActivity, str, Toast.LENGTH_SHORT).show();
+                int randomTemp = random.nextInt( WordsMap.getAnswerList().size());
+                mAnswer.setText(WordsMap.getAnswerList().get(randomTemp));
             }
         });
 
@@ -44,7 +53,9 @@ public class TwoChoiceFragment extends BaseFragment {
             }
         });
         pan.setItems(Constance.sExample);
-        pan.setmRepeatCount(40);
-        pan.setAnimalTime(10000);
+        pan.setmRepeatCount(30);
+        pan.setAnimalTime(6000);
+        mAnswer = root.findViewById(R.id.answer);
+
     }
 }
