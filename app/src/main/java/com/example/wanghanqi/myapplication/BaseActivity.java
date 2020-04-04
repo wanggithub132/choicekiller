24cc616b18b38c109a89776754e2c26d747780e8
wanghanqi <2268133030@qq.com>
2020-04-04 16:34:16 +0800

    权限申请

--- app/src/main/java/com/example/wanghanqi/myapplication/BaseActivity.java
+++ app/src/main/java/com/example/wanghanqi/myapplication/BaseActivity.java
@@ -1,14 +1,37 @@
 package com.example.wanghanqi.myapplication;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import com.baidu.mobstat.StatService;
+
+import static android.support.v4.content.PermissionChecker.PERMISSION_GRANTED;
 
 /**
  * Created by wanghanqi on 2020/2/6.
  */
 
 public class BaseActivity extends AppCompatActivity {
+
+    private static final int NOT_NOTICE = 2;//如果勾选了不再询问
+    private AlertDialog alertDialog;
+    private AlertDialog mDialog;
+    private static final String[] mPermissionList = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.INTERNET, Manifest.permission.READ_PHONE_STATE, Manifest.permission.ACCESS_FINE_LOCATION};
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkPermission();
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -21,5 +44,76 @@ public class BaseActivity extends AppCompatActivity {
         super.onPause();
         // 页面结束埋点，需要使用Activity的引用，以便代码能够统计到具体页面名
         StatService.onPause(this);
+
     }
+
+    /**
+     * 权限检查
+     */
+    private void checkPermission() {
+        ActivityCompat.requestPermissions(this, mPermissionList, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (grantResults[i] == PERMISSION_GRANTED) {//选择了“始终允许”
+
+                } else {
+                    if (!ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[i])) {//用户选择了禁止不再询问
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(BaseActivity.this);
+                        builder.setTitle("permission")
+                                .setMessage("点击允许才可以使用我们的app哦")
+                                .setPositiveButton("去允许", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        if (mDialog != null && mDialog.isShowing()) {
+                                            mDialog.dismiss();
+                                        }
+                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                        Uri uri = Uri.fromParts("package", getPackageName(), null);//注意就是"package",不用改成自己的包名
+                                        intent.setData(uri);
+                                        startActivityForResult(intent, NOT_NOTICE);
+                                    }
+                                });
+                        mDialog = builder.create();
+                        mDialog.setCanceledOnTouchOutside(false);
+                        mDialog.show();
+
+
+                    } else {//选择禁止
+                        AlertDialog.Builder builder = new AlertDialog.Builder(BaseActivity.this);
+                        builder.setTitle("permission")
+                                .setMessage("app的部分功能因为需要该权限才能正常运行，点击允许才可以使用app")
+                                .setPositiveButton("去允许", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        if (alertDialog != null && alertDialog.isShowing()) {
+                                            alertDialog.dismiss();
+                                        }
+                                        ActivityCompat.requestPermissions(BaseActivity.this,
+                                                mPermissionList, 1);
+                                    }
+                                });
+                        alertDialog = builder.create();
+                        alertDialog.setCanceledOnTouchOutside(false);
+                        alertDialog.show();
+                    }
+
+                }
+            }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == NOT_NOTICE) {
+            checkPermission();//由于不知道是否选择了允许所以需要再次判断
+        }
+    }
+
 }
